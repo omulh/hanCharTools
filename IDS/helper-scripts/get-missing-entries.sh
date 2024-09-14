fe487ce1820e8de62b61ffa5bf76c251086ea910
Omar L. <olh-acts@proton.me>
2024-09-14 17:01:45 +0200

    style: improve comments in get-missing-entries script

--- IDS/helper-scripts/get-missing-entries.sh
+++ IDS/helper-scripts/get-missing-entries.sh
@@ -5,27 +5,29 @@ TEMP_FILE=$(date +'tmp_%H%M%S')
 IDS_FILE='../IDS.TXT'
 cp "$IDS_FILE" "./$TEMP_FILE"
 
-# Extract all the Chinese characters that are
-# used in compositions in the IDS database
+# Use the copy to extract all the Chinese
+# characters that are used in compositions
+# for entries of the IDS database.
 # Remove comment lines
 sed -i '/#/d' "./$TEMP_FILE"
-# Remove the notes present in some lines
+# Remove the notes present in some entries
 sed -i "s/\t\*.*//" "./$TEMP_FILE"
 # Remove the text before the first ^ character, i.e.,
-# the unicode number and the character for that entry
+# the unicode number and the entry's character
 sed -i "s/^[^^]*\t^//" "./$TEMP_FILE"
 # Remove some unwanted characters
 sed -i 's/[A-Z0-9]//g' "./$TEMP_FILE"
 sed -i 's/[][\t$^(){}]//g' "./$TEMP_FILE"
 sed -i 's/[？〾⿰⿻⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿿㇯⿾⿼⿽]//g' "./$TEMP_FILE"
-# One char per line
+# Arrange to one character per line
 sed -i "s/./&\n/g" "./$TEMP_FILE"
 sed -i '/^$/d' "./$TEMP_FILE"
 # Remove duplicates
 LC_ALL=C sort -u "./$TEMP_FILE" -o "./$TEMP_FILE"
 
-# Check if the extracted chars are present in the IDS database
-lineCount=$(sed -n '$=' "$TEMP_FILE")
+# Check if the extracted chars have their
+# own entry in the IDS database
+lineCount=$(sed -n '$=' "./$TEMP_FILE")
 processCount=0
 while read testedChar; do
     ((processCount++))
@@ -40,4 +42,5 @@ while read testedChar; do
 done < "./$TEMP_FILE"
 echo -e "\r\033[0KProcessing done" >&2
 
+# Remove the temporary copy
 rm "./$TEMP_FILE"
