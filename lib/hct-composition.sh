ae5e79fbb09749a8fc7ae6c1e5a62ce16410434c
Omar L. <olh-acts@proton.me>
2024-09-18 10:22:17 +0200

    chore: update help texts in hct command scripts
    Add blank lines for more easier reading in hct-composition script.
    Add missing description for the 'source' option in hct-components script.

--- lib/hct-composition.sh
+++ lib/hct-composition.sh
@@ -25,8 +25,10 @@ Options:
                     when used, filter out composition options that do not contain the
                     specified source letter(s); see below to see the source options;
                     this option is ignored when using the 'wiktionary' option
+
   -w, --wiktionary  retrieve the composition information from Wiktionary
                     instead of the default local IDS database
+
   -h, --help        show this help message and exit
 
   The differen letters and the corresponding regions to be
